25b9d37f72076b1737152acceddb9849a149b327
gaetanopyro <gaetano.miscio@gmail.com>
2025-05-30 13:06:34 +0200

    add Player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Mainpage from "./assets/Components/Mainpage";
+import Player from "./assets/Components/Player";
 import Sidebar from "./assets/Components/Sidebar";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Mainpage />
+      <Player />
     </>
   );
 }
--- src/assets/Components/Player.jsx
+++ src/assets/Components/Player.jsx
@@ -1,4 +1,39 @@
+import { Col, Container, ProgressBar, Row } from "react-bootstrap";
+
 const Player = () => {
-  return <></>;
+  return (
+    <>
+      <Container fluid className="fixed-bottom bg-container pt-1">
+        <Row className="h-100">
+          <Col className="col-lg-10 offset-lg-2">
+            <Row className="h-100 flex-column justify-content-center align-items-center">
+              <Col className="col-6 col-md-4 playerControls">
+                <div className="d-flex">
+                  <a href="#">
+                    <img src="src\assets\PlayerBtn\shuffle.png" alt="shuffle" />
+                  </a>
+                  <a href="#">
+                    <img src="src\assets\PlayerBtn\prev.png" alt="prev" />
+                  </a>
+                  <a href="#">
+                    <img src="src\assets\PlayerBtn\play.png" alt="play" />
+                  </a>
+                  <a href="#">
+                    <img src="src\assets\PlayerBtn\next.png" alt="next" />
+                  </a>
+                  <a href="#">
+                    <img src="src\assets\PlayerBtn\repeat.png" alt="repeat" />
+                  </a>
+                </div>
+                <div className="progress mt-3">
+                  <ProgressBar role="progressbar"></ProgressBar>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 export default Player;
